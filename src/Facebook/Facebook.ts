97fa11f5f82d58616b40c37ba75320e2201758a1
Corentin Martin <corentin.martin@dynamicscreen.com>
2022-02-01 18:40:48 +0100

    logo color + asset downloading

--- src/Facebook/Facebook.ts
+++ src/Facebook/Facebook.ts
@@ -1,8 +1,8 @@
 import {
-    ISlideContext,
-    IPublicSlide,
-    SlideModule,
-    VueInstance
+  ISlideContext,
+  IPublicSlide,
+  SlideModule,
+  VueInstance, IAssetsStorageAbility, IAssetDownload, IVideoPlaybackAbility
 } from "dynamicscreen-sdk-js";
 
 import Post from "../Components/Post";
@@ -10,24 +10,56 @@ import PostAttachments from "../Components/PostAttachments";
 
 export default class FacebookSlideModule extends SlideModule {
     async onReady() {
-        return true;
+      // const guard = this.context.guardManager.add('ready', this.context.slide.id);
+      await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
+        //@ts-ignore
+        await ability.downloadAndGet(this.context.slide.data.page.picture.data.url, {
+          callback: (assetDownload: IAssetDownload) => {
+            assetDownload.onProgress.subscribe((progress, ev) => {
+              console.log('fb dl asset progress: ', progress)
+              ev.unsub();
+            });
+            assetDownload.onCompleted.subscribe((asset, ev) => {
+              console.log('fb dl asset completed')
+              ev.unsub();
+            });
+          }, noRetry: false
+        });
+
+        if (!!this.context.slide.data.post.attachments.data[0]?.media?.image?.src) {
+          await ability.downloadAndGet(this.context.slide.data.post.attachments.data[0].media.image.src);
+        }
+
+      });
+
+      return true;
     };
 
     setup(props: Record<string, any>, vue: VueInstance, context: ISlideContext) {
         const { h, reactive, ref, computed } = vue;
-
         const slide = reactive(this.context.slide) as IPublicSlide;
+
         const logo: string = "fab fa-facebook";
         const isPostWithAttachment = computed(() => {
-            return !!slide.data.post.media_url;
+            return !!slide.data.post.attachments.data[0]?.media?.image?.src;
         })
-        const postAttachment = isPostWithAttachment.value ? ref(slide.data.attachmentUrl) : null;
+        const postAttachment = ref(slide.data.post.attachments.data[0].media.image.src);
         const text = ref(slide.data.post.message);
         //@ts-ignore
         const userPicture = ref(slide.data.page.picture.data.url);
         //@ts-ignore
         const userName = ref(slide.data.page.username);
         const publicationDate = ref(slide.data.post.created_time);
+
+        this.context.onPrepare(async () => {
+          isPostWithAttachment.value && await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
+            postAttachment.value = await ability.getDisplayableAsset(postAttachment.value).then((asset) => asset.displayableUrl());
+          });
+
+          await this.context.assetsStorage().then(async (ability: IAssetsStorageAbility) => {
+            userPicture.value = await ability.getDisplayableAsset(userPicture.value).then((asset) => asset.displayableUrl());
+          });
+        });
 
         this.context.onPlay(async () => {
             this.context.anime({
